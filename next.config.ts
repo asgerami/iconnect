42ef4213ebb872df1892a83d12a3dfcfd23ec9ba
asgerami <amiraman467@gmail.com>
2025-06-13 12:22:22 +0300

    feat: configure image domains and remote patterns in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ['utfs.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        port: ''
+      }
+    ]
+  }
   /* config options here */
 };
 
