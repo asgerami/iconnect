4941add364738258c31ebbd36b4678131a03d06d
asgerami <amiraman467@gmail.com>
2025-06-13 12:17:41 +0300

    feat: implement EventDetails, UpdateEvent, and CreateEvent pages with event data fetching and forms

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,18 @@
+import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
+import { getAllEvents } from "@/lib/actions/event.actions";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const events = await getAllEvents({
+    query: "",
+    category: "",
+    page: 1,
+    limit: 6,
+  });
+  console.log(events);
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -16,24 +26,42 @@ export default function Home() {
               streamline the planning and execution of events of all sizes. From
               corporate conferences to intimate gatherings
             </p>
-            <Button size="lg" asChild className="rounded-full h-[54px] p-regular-16 w-full sm:w-fit">
+            <Button
+              size="lg"
+              asChild
+              className="rounded-full h-[54px] p-regular-16 w-full sm:w-fit"
+            >
               <Link href="#events">Explore Now</Link>
             </Button>
           </div>
           <Image
-          src="/assets/images/newhero.png"
-          alt="hero"
-          width={1000}
-          height={1000}
-          className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"/>
+            src="/assets/images/newhero.png"
+            alt="hero"
+            width={1000}
+            height={1000}
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+          />
         </div>
       </section>
-      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-        <h2 className="h2-bold">Trusted by <br /> Thousands of Events</h2>
+      <section
+        id="events"
+        className="wrapper my-8 flex flex-col gap-8 md:gap-12"
+      >
+        <h2 className="h2-bold">
+          Trusted by <br /> Event Organizers Across the Ethiopia
+        </h2>
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          Search
-          Category Filter
+          Search Category Filter
         </div>
+        <Collection
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={1}
+          totalPages={2}
+        />
       </section>
     </>
   );
